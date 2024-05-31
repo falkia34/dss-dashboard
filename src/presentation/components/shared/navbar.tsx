falkia34/db-dashboard

feat: Initial commit
Signed-off-by: Muhammad Naufal <me@falkia34.dev>

src/presentation/components/shared/navbar.tsx
@@ -1,0 +1,33 @@
+import { Avatar, List, Typography } from '@mui/material';
+import { NestedMenu, PathMenu, UrlMenu } from '@/domain/entities';
+import { NavbarDropdownMenu } from './navbar-dropdown-menu';
+import { NavbarMenu } from './navbar-menu';
+
+type Props = {
+  menus?: (PathMenu | UrlMenu | NestedMenu)[];
+};
+
+export function Navbar({ menus }: Props) {
+  return (
+    <nav className="flex flex-grow items-center justify-end md:ml-4">
+      <List dense={true} disablePadding={true} className="hidden text-center md:flex">
+        {menus &&
+          menus.map((menu, i) => {
+            if (menu.hasOwnProperty('path') || menu.hasOwnProperty('url')) {
+              return <NavbarMenu key={i} menu={menu as PathMenu | UrlMenu} />;
+            } else {
+              return <NavbarDropdownMenu key={i} menu={menu as NestedMenu} />;
+            }
+          })}
+      </List>
+      <Typography className="hidden mr-4 md:block">Muhammad Naufal</Typography>
+      <Avatar
+        variant="circular"
+        sizes="32px"
+        src="/assets/img/profile.png"
+        alt="Muhammad Naufal"
+        className="m-1 w-8 h-8 md:m-0"
+      />
+    </nav>
+  );
+}

falkia34/dss-dashboard

src/presentation/components/shared/navbar.tsx
@@ -1,0 +1,33 @@
+import { Avatar, List, Typography } from '@mui/material';
+import { NestedMenu, PathMenu, UrlMenu } from '@/domain/entities';
+import { NavbarDropdownMenu } from './navbar-dropdown-menu';
+import { NavbarMenu } from './navbar-menu';
+
+type Props = {
+  menus?: (PathMenu | UrlMenu | NestedMenu)[];
+};
+
+export function Navbar({ menus }: Props) {
+  return (
+    <nav className="flex flex-grow items-center justify-end md:ml-4">
+      <List dense={true} disablePadding={true} className="hidden text-center md:flex">
+        {menus &&
+          menus.map((menu, i) => {
+            if (menu.hasOwnProperty('path') || menu.hasOwnProperty('url')) {
+              return <NavbarMenu key={i} menu={menu as PathMenu | UrlMenu} />;
+            } else {
+              return <NavbarDropdownMenu key={i} menu={menu as NestedMenu} />;
+            }
+          })}
+      </List>
+      <Typography className="hidden mr-4 md:block">DSS User</Typography>
+      <Avatar
+        variant="circular"
+        sizes="32px"
+        src="/assets/img/profile.png"
+        alt="DSS User"
+        className="m-1 w-8 h-8 md:m-0"
+      />
+    </nav>
+  );
+}