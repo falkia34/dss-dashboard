5e235e61c206b7f4333a8b0ba00da7f02802d130
Muhammad Naufal <me@falkia34.dev>
2024-06-04 19:11:45 +0700

    fix(datasource): Fix local and session data sources
    Signed-off-by: Muhammad Naufal <me@falkia34.dev>

--- src/infrastructure/datasources/local-storage.data-source.ts
+++ src/infrastructure/datasources/local-storage.data-source.ts
@@ -1,25 +1,30 @@
+'use client';
+
 import { injectable } from 'inversify';
 
 export interface LocalStorageDataSource {
-  get<T>(key: string): T;
+  get<T>(key: string): T | null;
   set<T>(key: string, value: T): void;
   remove(key: string): void;
 }
 
 @injectable()
 export class LocalStorageDataSourceImpl implements LocalStorageDataSource {
-  public get<T>(key: string): T {
-    'use client';
-    return JSON.parse(window.localStorage.getItem(key) || '{}');
+  public get<T>(key: string): T | null {
+    let value = null;
+
+    if (typeof window !== 'undefined') {
+      value = window.localStorage.getItem(key);
+    }
+
+    return value ? JSON.parse(value) : null;
   }
 
   public set<T>(key: string, value: T): void {
-    'use client';
     window.localStorage.setItem(key, JSON.stringify(value));
   }
 
   public remove(key: string): void {
-    'use client';
     window.localStorage.removeItem(key);
   }
 }
